0a80e116b42a434ef4fa59edf3740d76f8323a8e
LKleemann <43823472+LKleemann@users.noreply.github.com>
2019-11-20 13:35:21 +0100

    Update details.php - RaspberryPI Update

--- web/details.php
+++ web/details.php
@@ -1,9 +1,9 @@
 <?php
+require_once __DIR__ . '/init.php';
+require_once __DIR__ . '/inc/config.inc.php';
+require_once __DIR__ . '/inc/functions.inc.php';
 
-require_once($root_url."/inc/config.inc.php");
-require_once($root_url."/inc/functions.inc.php");
-
-include($root_url."/templates/header.inc.php")
+include __DIR__ . '/templates/header.inc.php'
 ?>
 
       <div class="row">
@@ -60,7 +60,7 @@ include($root_url."/templates/header.inc.php")
 
 
 <?php
-include($root_url."/templates/footer.inc.php")
+include __DIR__ . '/templates/footer.inc.php';
 ?>
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
