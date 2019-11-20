7cb3cd141b485e999356b68a406a1fe1e0df37ea
LKleemann <43823472+LKleemann@users.noreply.github.com>
2019-11-20 13:36:58 +0100

    Update config.php - RaspberryPI Update

--- web/inc/config.inc.php
+++ web/inc/config.inc.php
@@ -7,7 +7,7 @@
 //Tragt hier eure Verbindungsdaten zur Datenbank ein
 $db_host = 'localhost';
 $db_name = 'bkwi1920';
-$db_user = 'root';
-$db_password = '';
+$db_user = 'bkwi';
+$db_password = '5lKFZFK5eAjmNJB5';
 
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
