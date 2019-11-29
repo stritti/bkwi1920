88cd061bd703166ef8c9a4e9af9d636527c6b501
Lukas Kleemann <Lukas.Kleemann@gmx.de>
2019-11-29 20:44:37 +0100

    Preparation for Christmas - added dirname PHP_SELF

--- web/templates/header.inc.php
+++ web/templates/header.inc.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Loginscript</title>
     <!-- Bootstrap core CSS -->
-    <link href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../vendor/bootstrap/css/bootstrap.min.css';?>" rel="stylesheet">
+    <link href="<?php echo dirname($_SERVER['PHP_SELF']) . '/vendor/bootstrap/css/bootstrap.min.css';?>" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="<?php echo dirname($_SERVER['PHP_SELF']) . '/../../css/styles.css';?>" rel="stylesheet">
+    <link href="<?php echo dirname($_SERVER['PHP_SELF']) . '/css/styles.css';?>" rel="stylesheet">
   </head>
   <body>
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-    <a class="navbar-brand" href="<?php echo __DIR__ . '/../../index.php';?>">Loginscript</a>
+    <a class="navbar-brand" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/index.php';?>">Loginscript</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -22,10 +22,10 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo __DIR__ . '/../../user/login.php';?>">Anmelden</a>
+            <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/user/login.php';?>">Anmelden</a>
           </li>
           <li class="nav-item">
-          <a class="nav-link" href="<?php echo __DIR__ . '/../../user/register.php';?>">Register</a>
+          <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/user/register.php';?>">Register</a>
           </li>
         </ul>
       </div>
@@ -33,14 +33,14 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo __DIR__ . '/../../admin/internal.php';?>">Datenbank
+            <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/admin/internal.php';?>">Datenbank
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo __DIR__ . '/../../user/settings.php';?>">Einstellungen</a>
+            <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/user/settings.php';?>">Einstellungen</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo __DIR__ . '/../../user/logout.php';?>">Abmelden</a>
+            <a class="nav-link" href="<?php echo dirname($_SERVER['PHP_SELF']) . '/user/logout.php';?>">Abmelden</a>
           </li>
         </ul>
       </div>
