4e73d2cb6c731d8f5fc3c791b1c62dadc0ea9b1e
Lukas Kleemann <Lukas.Kleemann@gmx.de>
2019-11-29 21:39:34 +0100

    Preparation for Christmas - index - MYSQL

--- web/index.php
+++ web/index.php
@@ -39,20 +39,16 @@ while($row = $statement->fetch()) {
         <div class="col-md-4 mb-5">
             <div class="card h-100">
             <img class="card-img-top" src="http://placehold.it/300x200" alt="">
-            <div class="card-body">
-                <?php while($row = $statement->fetch()){ ?>
-                    <h4 class='card-title'><?php echo $row['title']; ?> </h4>";
-                        <div class="card-text col">
-                        Preis
-                <?php } ?>
-                <div class="row">
-                    <div class="card-text col"><?php echo $row['price']; ?></div>
-                    <div class="card-text col"><?php echo $row['createdate']; ?></div>
+                <div class="card-body">
+                        <h4 class='card-title'><?php echo $row['title']; ?> </h4>";
+                    <div class="row">
+                        <div class="card-text col"><?php echo $row['price']; ?></div>
+                        Preparation for Christmas     <div class="card-text col"><?php echo $row['createdate']; ?></div>
+                    </div>
                 </div>
-            </div>
-            <div class="card-footer">
-                <a href="angebot.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Beobachtung</a>
-            </div>
+                <div class="card-footer">
+                    <a href="angebot.php?id=<?php echo $row['id']; ?>" class="btn btn-primary">Beobachtung</a>
+                </div>
             </div>
         </div>
 <?php
