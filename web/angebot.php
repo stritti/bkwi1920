301fc067988a18149244bf0ef1305c22c1e49d3f
Rosenberg Julian <56641884+virtuosomcmember@users.noreply.github.com>
2019-12-11 15:26:19 +0100

    neu

--- web/angebot.php
+++ web/angebot.php
@@ -11,58 +11,64 @@ $statement->execute(array(':id' => $_GET['id']));
 while($row = $statement->fetch()) {
 ?>
 
-<h1><?php echo $row['title'];?> <span class="badge badge-secondary">New</span></h1>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://via.placeholder.com/350x128" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://via.placeholder.com/350x128" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://via.placeholder.com/350x128" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-<p class="text-center" style="font-size: 70pt;"><?php echo $row['price'];?> €</p>
-
 <div class="container">
-<div class="row justify-content-center">
-    <div class="col-sm-4">
-      <?php echo $row['nachname'] . ", " . $row['vorname'];?>
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="https://via.placeholder.com/350x128" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="https://via.placeholder.com/350x128" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+        <img src="https://via.placeholder.com/350x128" class="d-block w-100" alt="...">
+        </div>
     </div>
-    <div class="col-sm-4">
-     <?php echo $row['class'];?>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
     </div>
-  </div>
-    </div>
+
+
+    <h1><?php echo $row['title'];?> <span class="badge badge-secondary">New</span></h1>
+
+
+    <p class="text-center" style="font-size: 50pt;"><?php echo $row['price'];?> €</p>
+
     <div class="row justify-content-center">
-    <div class="col-sm-4">
-<button type="button" class="btn btn-secondary">
-Beobachten  <span class="badge badge-warning">0</span>
-</button>
+
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><?php echo $row['nachname'] . ", " . $row['vorname'];?></li>
+            <li class="list-group-item"><?php echo $row['class'];?></li>
+        </ul>
+
+        <div class="row justify-content-center">
+            <div class="col-sm-4">
+                <button type="button" class="btn btn-secondary">
+                    Beobachten  <span class="badge badge-warning">0</span>
+                </button>
+            </div>
+            <div class="">
+                <input class="btn btn-secondary" type="submit" value="Interesse" />
+            </div>
+        </div>
+
+        <p class="text-center"><?php echo $row['description'];?></p>
+
     </div>
-        <div class="col-sm-">
-<input class="btn btn-secondary" type="submit" value="Interesse">
-      </div>
-      </div>
 
-      <p class="text-center"><?php echo $row['description'];?></p>
-
+</div>
 <?php
 }
 ?>
